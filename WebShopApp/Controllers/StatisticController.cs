defd9718540dc55ac5a56d71d5dd144800c7f248
IvoIK <ik26@abv.bg>
2025-01-29 09:04:56 +0200

    Update WebShopApp - Statistics

---
+++ WebShopApp/Controllers/StatisticController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebShopApp.Core.Contracts;
+using WebShopApp.Models.Statistic;
+
+namespace WebShopApp.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private readonly IStatisticService _statisticsService;
+
+        public StatisticController(IStatisticService statisticsService)
+        {
+            this._statisticsService = statisticsService;
+        }
+
+        public IActionResult Index()
+        {
+            StatisticVM statistics = new StatisticVM();
+
+            statistics.CountClients = _statisticsService.CountClients();
+            statistics.CountProducts = _statisticsService.CountProducts();
+            statistics.CountOrders = _statisticsService.CountOrders();
+            statistics.SumOrders = _statisticsService.SumOrders();
+
+            return View(statistics);
+        }
+    }
+}
